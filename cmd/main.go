8075c2ab2c1eee6efffbcc75111c7f431eef7bb3
farhaanali20 <mohdfarhaanali20@gmail.com>
2022-11-14 17:40:46 +0530

    most complete

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoute(r)
+	http.Handle("/",r)
+	log.Fatal(http.ListenAndServe("localhost:9010",r))
+}
